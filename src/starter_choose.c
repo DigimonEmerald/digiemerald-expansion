8048c4b7447e292bedee877cc008a2ab1c81e2d4
Luna3Step <lunagooch02@gmail.com>
2024-07-29 15:20:25 +0100

    Change Starters

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -112,9 +112,9 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_SUNARZAMON,
-    SPECIES_SYAKOMON_X,
-    SPECIES_TERRIERMON,
+    SPECIES_PUNIMON,
+    SPECIES_KOROMON,
+    SPECIES_YOKOMON,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
