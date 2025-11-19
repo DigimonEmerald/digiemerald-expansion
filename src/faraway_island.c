d2718f8ac8dec538cbce990f5375c4d857484ec1
Luna3Step <lunagooch02@gmail.com>
2025-11-19 15:28:41 +0000

    Merge Fixes

--- src/faraway_island.c
+++ src/faraway_island.c
@@ -393,7 +393,7 @@ void SetFalcomonAboveGrass(void)
 
         LoadSpritePalette(&gSpritePalette_GeneralFieldEffect1);
         UpdateSpritePaletteWithWeather(IndexOfSpritePaletteTag(gSpritePalette_GeneralFieldEffect1.tag), FALSE);
-        gSprites[mew->spriteId].subspriteTableNum = 1;
+        gSprites[falcomon->spriteId].subspriteTableNum = 1;
 
         x = falcomon->currentCoords.x;
         y = falcomon->currentCoords.y;
